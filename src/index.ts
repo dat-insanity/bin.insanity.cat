4581ccf17225fc13afb4781b649210081a74a64d
dat-insanity <dat@insanity.cat>
2023-12-11 03:47:24 +1100

    feat: Add validation for environment variables and .env file existence

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import router from "./utils/router.ts";
 
-Bun.serve({
-  port: Bun.env.PORT,
-  hostname: Bun.env.HOSTNAME,
-  fetch(req) {
-    return router(req);
-  },
-});
+if (!(await Bun.file(".env").exists())) {
+  throw new Error("Missing .env file. A template is provided at .env.example.");
+}
+
+if (
+  Bun.env.PORT &&
+  Bun.env.HOSTNAME &&
+  Bun.env.PROTOCOL &&
+  Bun.env.SERVER_URL
+) {
+  Bun.serve({
+    port: Bun.env.PORT,
+    hostname: Bun.env.HOSTNAME,
+    fetch(req) {
+      return router(req);
+    },
+  });
+} else {
+  throw new Error(
+    "Missing environment variables. A template is provided at .env.example."
+  );
+}
