0f46af06d608e9be29c5ab527117a2239159d096
dat-insanity <dat@insanity.cat>
2023-12-23 04:29:01 +1100

    feat: created paste view

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,12 +1,15 @@
 import create from "../routes/create";
 import raw from "../routes/raw";
 import index from "../routes/index";
+import view from "../routes/view";
 
 export default function router(req: Request) {
   const url = new URL(req.url);
   if (url.pathname === "/create") return create(req);
   if (url.pathname.startsWith("/raw/")) return raw(req);
   if (url.pathname === "/") return index(req);
+  if (url.pathname.split("/").length == 2 && url.pathname.split("/")[1] !== "")
+    return view(req);
 
   return new Response("404: Page not found.", { status: 404 });
 }
